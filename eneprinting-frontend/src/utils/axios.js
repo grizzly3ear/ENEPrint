0cad348e6d15ae30a572902e9df887a0f1dd4188
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-13 18:47:03 +0700

    [UPDATE] add cors

--- eneprinting-frontend/src/utils/axios.js
+++ eneprinting-frontend/src/utils/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'http://localhost:8000',
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
 })
 export default instance
--- eneprinting/app/Http/Kernel.php
+++ eneprinting/app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
 
         'api' => [
@@ -61,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class
     ];
 
     /**
