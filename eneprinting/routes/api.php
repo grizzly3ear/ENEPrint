7998ef2842b53f5f9d34e8a239f883f51409ad0f
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-15 02:15:55 +0700

    [UPDATE] api / route for delete course from user

--- eneprinting/routes/api.php
+++ eneprinting/routes/api.php
@@ -2,26 +2,20 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/login', 'AuthenticationController@login');
 Route::post('/register', 'AuthenticationController@register');
+
 Route::get('/profile/{user_id}', 'ProfileController@showProfile');
+Route::get('/profile/{user_id}/course', 'ProfileController@listCourseByUser');
+Route::post('/profile/{user_id}/course', 'ProfileController@storeCourseToUser');
 
 Route::get('course', 'CourseController@listCourse');
 Route::post('course', 'CourseController@addCourse');
 Route::get('course/search', 'CourseController@searchCourse');
+
+Route::delete('instructor-course/{instructor_has_course_id}', 'CourseController@deleteCourseFromUser');
 
