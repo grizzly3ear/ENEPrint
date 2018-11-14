c7af779fa20af519cd5ec9ecaf83bfda8591d7b9
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-15 02:27:17 +0700

    [ADD] frontend / add env for development, production mode

--- eneprinting-frontend/src/utils/axios.js
+++ eneprinting-frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: `${process.env.VUE_APP_AXIOS_URL}`,
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
