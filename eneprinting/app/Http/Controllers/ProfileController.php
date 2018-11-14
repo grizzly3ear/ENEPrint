6910ea0a622049e8786b760f6c61b5ec18143f99
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-15 02:15:38 +0700

    [UPDATE] api / add store course to user, list course by user

--- eneprinting/app/Http/Controllers/ProfileController.php
+++ eneprinting/app/Http/Controllers/ProfileController.php
@@ -5,12 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\User;
-use App\Instructor;
+use App\Course;
 
 class ProfileController extends Controller
 {
     public function showProfile($user_id){
         return User::find($user_id)->instructorProfile;
+    }
+
+    public function storeCourseToUser(Request $request, $user_id){
+        $instructor = User::find($user_id)->instructorProfile;
+        $course = Course::find($request->course_id);
+        $instructor->courses()->save($course);
+        return $course;
+    }
+
+    public function listCourseByUser($user_id){
+        $courses = User::find($user_id)->instructorProfile->courses;
+        return $courses;
     }
 
 }
