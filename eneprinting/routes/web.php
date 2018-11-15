f30de32e087df7b94e968b46c134d988161f18cc
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-15 11:40:07 +0700

    [UPDATE] api / add redundant route

--- eneprinting/routes/web.php
+++ eneprinting/routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('test', function () {
+    return 'helloword';
+});
+
+Route::post('/login', 'AuthenticationController@login');
+Route::post('/register', 'AuthenticationController@register');
+
+Route::get('/profile/{user_id}', 'ProfileController@showProfile');
+Route::get('/profile/{user_id}/course', 'ProfileController@listCourseByUser');
+Route::post('/profile/{user_id}/course', 'ProfileController@storeCourseToUser');
+
+Route::get('course', 'CourseController@listCourse');
+Route::post('course', 'CourseController@addCourse');
+Route::get('course/search', 'CourseController@searchCourse');
+
+Route::delete('instructor-course/{instructor_has_course_id}', 'CourseController@deleteCourseFromUser');
