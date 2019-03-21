36a8fc2e0783d258d990785985943afcba5120b3
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-03-21 10:52:32 +0700

    [UPDATE] api / set default ordering to Course by year

--- eneprinting/app/Course.php
+++ eneprinting/app/Course.php
@@ -3,20 +3,35 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'course';
 
-    public function instructors(){
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('year', 'asc');
+        });
+    }
+
+    public function instructors()
+    {
         return $this->belongsTomany('App\Instructor', 'instructor_has_course', 'course_id', 'instructor_id')->withPivot('id');
     }
 
-    public function students(){
+    public function students()
+    {
         return $this->belongsToMany('App\Student', 'course_plan', 'course_id', 'student_id')->withPivot('id');
     }
 
-    public function documents(){
+    public function documents()
+    {
         return $this->hasMany('App\Document', 'course_id');
     }
 }
