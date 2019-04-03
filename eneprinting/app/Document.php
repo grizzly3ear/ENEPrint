c124d6959e3ade173a6950adbe7ea52c977bba8d
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-03 10:25:05 +0700

    [UPDATE] api / order by created_at by default

--- eneprinting/app/Document.php
+++ eneprinting/app/Document.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Document extends Model
 {
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('created_at', 'desc');
+        });
+    }
+
     protected $table = 'document';
 
     public function course(){
