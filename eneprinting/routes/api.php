6a930c74bfbd9cc4095841d34c1c67f984373a6d
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-05-28 11:28:14 +0700

    [UPDATE] api / add route for update remain_money

--- eneprinting/routes/api.php
+++ eneprinting/routes/api.php
@@ -14,6 +14,7 @@ Route::get('/profile/{user_id}/course', 'ProfileController@listCourseByUser');
 Route::post('/profile/{user_id}/course', 'ProfileController@storeCourseToUser');
 Route::post('/profile/{user_id}/upload', 'ProfileController@storeFileToCourse');
 Route::get('/profile/{user_id}/file', 'ProfileController@listFile');
+Route::put('/profile/{user_id}/remain-money', 'ProfileController@updateRemainMoney');
 
 Route::get('course', 'CourseController@listCourse');
 Route::post('course', 'CourseController@addCourse');
